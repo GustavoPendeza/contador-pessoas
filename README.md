2165cedb0168fafd3c09f26598811c78047df605
Gustavo S. Pendeza <g.pendeza@hotmail.com>
2022-07-27 15:42:29 -0300

    Adicionando projeto ao GitHub

---
+++ README.md
@@ -0,0 +1,26 @@
+# Contador de Pessoas
+
+Projeto de um contador de pessoas dentro de um ambiente.
+
+## Sobre
+
+Com a chegada da pandemia as pessoas tiveram que controlar o número de pessoas dentro dos estabelecimentos, e para facilitar isso existem aplicativos que ajudam a contar quantas pessoas já entraram ou saíram desses lugares.
+
+Esse contador tem um limite máximo de 20 pessoas dentro do estabelecimento.
+
+## Tecnologias
+
+<img alt="Flutter" title="Flutter" src="https://img.shields.io/badge/Flutter-02569B?style=for-the-badge&logo=flutter&logoColor=white">
+
+<img alt="Dart" title="Dart" src="https://img.shields.io/badge/Dart-0175C2?style=for-the-badge&logo=dart&logoColor=white">
+
+## Tela do App
+
+<img alt="Tela App" title="Tela App" height="500em" src="https://user-images.githubusercontent.com/53589614/181345497-a3682080-075c-4e5f-9b2c-6665ee612ae1.jpeg">
+<img alt="Tela App Lotado" title="Tela App Lotado" height="500em" src="https://user-images.githubusercontent.com/53589614/181346101-ea3e7785-513b-4e20-ac79-f681d06bfbf2.jpeg">
+
+## Imagem de fundo
+<a href='https://www.freepik.com/photos/bar-background'>Bar background photo created by ijeab - www.freepik.com</a>
+
+## Ícone utilizado como ícone do App
+<a href="https://www.flaticon.com/br/icones-gratis/adicionar" title="adicionar ícones">Adicionar ícones criados por Freepik - Flaticon</a>
---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  // Remove pessoas
+  void decrement() {
+    setState(() {
+      count--;
+    });
+  }
+
+  // Adiciona pessoas
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+  
+  // Verifica se pessoas é igual a zero
+  bool get isEmpty => count == 0;
+  // Verifica se pessoas é igual a 20
+  bool get isFull => count == 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/image.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+              Text(
+              isFull ? 'Lotado!' : 'Pode entrar!',
+              style: TextStyle(
+                fontSize: 30,
+                color: isFull ? Colors.red : Colors.white,
+                fontWeight: FontWeight.w700,
+                shadows: const [
+                  Shadow(
+                      // bottomLeft
+                      offset: Offset(-1.5, -1.5),
+                      color: Colors.black),
+                  Shadow(
+                      // bottomRight
+                      offset: Offset(1.5, -1.5),
+                      color: Colors.black),
+                  Shadow(
+                      // topRight
+                      offset: Offset(1.5, 1.5),
+                      color: Colors.black),
+                  Shadow(
+                      // topLeft
+                      offset: Offset(-1.5, 1.5),
+                      color: Colors.black),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Text(
+                count.toString(),
+                style: TextStyle(
+                  fontSize: 100,
+                  color: isFull ? Colors.red : Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 32,
+                ),
+                TextButton(
+                  onPressed: isFull ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
